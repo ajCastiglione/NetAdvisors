b3b93ab701d9076c2c21e08c260252b34bbb20d8
Antonio Castiglione <antonio@minervawebdevelopment.com>
2025-08-21 10:46:23 -0400

    fixed grid cols

--- home.php
+++ home.php
@@ -33,7 +33,7 @@ global $wp_query;
 		?>
 
 		<div class="relative container py-12">
-			<div class="grid grid-cols-3 gap-6 lg:gap-8 mb-12">
+			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 mb-12">
 				<?php
 				if ( have_posts() ) :
 					while ( have_posts() ) :
